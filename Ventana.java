c8dae4423f2af8b43bfcc91f0cf94c1c20dd98d3
Josu-05 <josuevillalva05@hotmail.com>
2025-04-12 23:21:26 -0500

    Add files via upload

--- Ventana.java
+++ Ventana.java
@@ -4,47 +4,56 @@ import java.awt.event.ActionListener;
 
 public class Ventana {
     private JPanel principal;
-    private JButton btnInsertar;
-    private JButton btnExtraer;
-    private JTextArea txtListado;
-    private JLabel lblEtiqueta;
-    private JTextField txtDato;
-    private JButton btnDevolver;
-    private Pila coleccion=new Pila();
+    private JComboBox cboModelo;
+    private JTextField txtAnio;
+    private JButton btnAgregar;
+    private JButton btnDesencolar;
+    private JTextArea txtListarAutos;
+    private JLabel lblMensaje;
+    private ColaAutos listaAutos = new ColaAutos();
 
-    public Ventana(){
-
-        btnInsertar.addActionListener(new ActionListener() {
-            int contador = 0;
+    public Ventana() {
+        btnAgregar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    if (contador < 10) {
-                        coleccion.insertar(txtDato.getText());
-                        contador++;
-                        txtListado.setText(coleccion.toString());
-                    } else {
-                        throw new IllegalArgumentException("Unicamente se pueden poner 10 objetos");
+                    String modelo = cboModelo.getSelectedItem().toString();
+                    int anio = Integer.parseInt(txtAnio.getText());
+
+                    if(txtAnio.bounds().isEmpty()){
+                        throw new Exception("El año no puede estar vacio");
                     }
-                }catch (IllegalArgumentException ex){
-                    JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
+                    if(anio < 0){
+                        throw new Exception("El año no puede ser negativo");
+                    }
+
+                    if(anio > 2025){
+                        throw new Exception("El año no puede ser mayor a 2025");
+                    }
+
+                    listaAutos.encolar(new Auto(modelo, anio));
+                    txtListarAutos.setText(listaAutos.listarAutos());
+                    JOptionPane.showMessageDialog(null,"El auto se agrego al carrito");
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null,"El debe ser un numero valido");
+                }catch (Exception ex){
+                    JOptionPane.showMessageDialog(null,ex.getMessage());
                 }
             }
         });
-        btnExtraer.addActionListener(new ActionListener() {
+        btnDesencolar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String eliminado=coleccion.extraer();
-                JOptionPane.showMessageDialog(null,"Se elimino: "+eliminado);
-                txtListado.setText(coleccion.toString());
-            }
-        });
-        btnDevolver.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String extraido =coleccion.cima();
-                JOptionPane.showMessageDialog(null, "Objeto extraido: "+extraido);
-                txtListado.setText(coleccion.toString());
+                try{
+                    Auto autox = listaAutos.desencolar();
+                    int antiguedad = 2025 - autox.getAnio();
+                    int totalPagar = 50 + (antiguedad * 50);
+                    txtListarAutos.setText(listaAutos.listarAutos());
+                    lblMensaje.setText("Auto atendido " + autox + "Debe pagar: " + totalPagar );
+                }catch (Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
             }
         });
     }
@@ -53,7 +62,8 @@ public class Ventana {
         JFrame frame = new JFrame("Ventana");
         frame.setContentPane(new Ventana().principal);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        //frame.pack();
+        frame.setSize(600,400);
         frame.setVisible(true);
     }
 }
